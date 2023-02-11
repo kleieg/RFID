9872f5e3f8020e17a3c1a701edd5531a3673a948
kleieg <egbert.kleinhans@gmail.com>
2023-02-11 13:44:08 +0100

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,36 @@
 #include <Arduino.h>
 
-#include <Wire.h>
-#include <Adafruit_MLX90614.h>
-#include <Adafruit_SSD1306.h>
+#include <SPI.h>
+#include <MFRC522.h>
+ 
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+ 
+void setup() 
+{
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-Adafruit_MLX90614 mlx = Adafruit_MLX90614();
-
-void setup() {
-  
-  Serial.begin(9600);
-  Serial.println("Adafruit MLX90614 test"); 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 init failed"));
-    for(;;);
+}
+void loop() 
+{
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
   }
-  
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  Serial.println();
 
-  display.clearDisplay();
-  display.setRotation(2);
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 35);
-  display.println("Init...");
-  display.display();
-  delay(500);
-  display.clearDisplay();
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
 
-  mlx.begin(); 
-  
-}
+} 
 
-void loop() {
-
-    display.clearDisplay();
-
-    display.setTextSize(3);
-    display.setCursor(5, 27);
-    display.print(mlx.readObjectTempC()); //IR-Temperatur
-    display.setCursor(100, 27);
-    display.print("C");
-    display.display();
-
-
-  Serial.print("Umgebungstemperatur: ");
-  Serial.println(mlx.readAmbientTempC());
-  Serial.print("IR-Temperatur: ");
-  Serial.println(mlx.readObjectTempC());
-  delay(1000);
-}
