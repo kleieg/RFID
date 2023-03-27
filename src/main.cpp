c51759520b4fff00a17ca33274c0f4945a65b9be
kleieg <egbert.kleinhans@gmail.com>
2023-03-27 20:27:11 +0200

    version mit Inpu Menu

--- src/main.cpp
+++ src/main.cpp
@@ -6,31 +6,78 @@
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+int cmd;
+int numbyte;
+int menu = 1;
  
 void setup() 
 {
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Approximate your card to the reader...");
-  Serial.println();
+
 
 }
 void loop() 
 {
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
-    return;
+  if (menu) {
+    Serial.println("1 - Karte lesen ( Karte muss bereits aufliegen!!)");
+    Serial.println("2 - Karte schreiben");
+    Serial.print("Auswahl:");
+    menu = 0;
   }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
-    return;
+
+  if (Serial.available() > 0) { // if there is data 
+    numbyte = Serial.available();
+
+    if (numbyte > 1){
+      Serial.println("Eingabe zu lang!");  // tritt nie auf!
+    }
+    cmd =  Serial.read() - 48;
+
+
+    switch (cmd) {
+      case 1:
+        // Look for new cards
+        if ( ! mfrc522.PICC_IsNewCardPresent()) 
+        {
+          Serial.println();
+          Serial.println();
+          Serial.println("keine Karte oder Karte bereits gelesen.");
+          Serial.println();
+          menu = 1;
+          return;
+        }
+        // Select one of the cards
+        if ( ! mfrc522.PICC_ReadCardSerial()) 
+        {
+          Serial.println();
+          Serial.println();
+          Serial.println("?? unklar??");
+          Serial.println();
+          menu = 1;
+          return;
+        }
+
+        mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+        
+        Serial.println();
+        Serial.println();
+        Serial.println("Karte wird nur einmal gelesen!");
+        Serial.println("zum erneuten lesen: entfernen und erneut auflegen.");
+        Serial.println();
+        Serial.println();
+        menu = 1;
+      break;
+      default:
+        Serial.println();
+        Serial.println();
+        Serial.println("falsche Auswahl!");
+        Serial.println();
+        Serial.println();
+        menu = 1;
+    }
   }
-  Serial.println();
-
-  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
-
 } 
 
